5868baf4fcfde3afc73980e90360d4402c21faf9
none <none@none.com>
2026-06-16 18:31:42 -0400

    added ec2 module

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "subnet_1_id" {
+  type        = string
+  description = "Subnet 1 id"
+}
+
+variable "subnet_2_id" {
+  type        = string
+  description = "Subnet 2 id"
+}
+
+variable "subnet_3_id" {
+  type        = string
+  description = "Subnet 3 id"
+}
+
+variable "ssm_profile_name" {
+  type        = string
+  description = "ssm profile name"
+}
