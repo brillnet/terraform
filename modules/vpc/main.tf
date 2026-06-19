87042a243cea6a7241d63c6aed2abf0162a03c49
none <none@none.com>
2026-06-19 15:36:10 -0400

    removed comment

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -75,7 +75,6 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   security_group_id = aws_security_group.allow_ssh.id
   cidr_ipv4         = "0.0.0.0/0"
-  # cidr_ipv4         = "18.206.107.24/29"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
